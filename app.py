2d89b7bbb88bd36fe0b1c6b41e41d8ed853f8b42
Rdxmax <rahul9911279462@gmail.com>
2021-02-13 16:33:31 +0530

    Added Login and signup functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4ee9ee6f97edadd4dc2cb968045adafc'
 
 posts = [
 	{
@@ -37,6 +39,25 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'test@admin.com' and form.password.data == 'test':
+			flash('Loggen In Successfully', 'green')
+			return redirect(url_for('home'))
+		else:
+			flash('Log In unsuccessful', 'red')
+	return render_template("login.html", form=form)
+
+@app.route("/signup", methods=['GET','POST'])
+def signup():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash('Account created successfully!', 'red')
+		return redirect(url_for('home'))
+	return render_template("signup.html", form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
