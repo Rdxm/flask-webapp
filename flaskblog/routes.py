f27845c529971c43a6205e3c37d38f4b8178edea
Rahul Chaudhary <rahul.chaudhary.3100@gmail.com>
2021-03-05 08:36:31 +0530

    User authentication

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, redirect, flash
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flaskblog import app
+from flaskblog import app, bcrypt, db
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 	{
@@ -41,9 +42,13 @@ def about():
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'test@admin.com' and form.password.data == 'test':
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
 			flash('Logged In Successfully', 'green')
 			return redirect(url_for('home'))
 		else:
@@ -52,8 +57,25 @@ def login():
 
 @app.route("/signup", methods=['GET','POST'])
 def signup():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = RegistrationForm()
 	if form.validate_on_submit():
+		pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=pwd)
+		db.session.add(user)
+		db.session.commit()
 		flash('Account created successfully!', 'green')
-		return redirect(url_for('home'))
+		return redirect(url_for('login'))
 	return render_template("signup.html", form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+	return render_template("account.html")
