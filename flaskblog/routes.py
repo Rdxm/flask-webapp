e3262349e8bda29a7e0af463b3b49affe189edfb
Rahul Chaudhary <rahul.chaudhary.3100@gmail.com>
2021-03-07 11:56:26 +0530

    User Account and Profile Picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, url_for, redirect, flash
-from flaskblog.forms import RegistrationForm, LoginForm
+import os
+from flask import render_template, url_for, redirect, flash, request
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flaskblog import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+from PIL import Image
 
 posts = [
 	{
@@ -34,6 +37,7 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
+	# image_file = url_for('static', filename='img/'+current_user.image_file)
 	return render_template("index.html", posts=posts)
 
 @app.route("/about")
@@ -75,7 +79,37 @@ def logout():
 	logout_user()
 	return redirect(url_for('home'))
 
-@app.route("/account")
+def save_picture(form_picture):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	prev_picture = os.path.join(app.root_path, 'static/img', current_user.image_file)
+	if os.path.exists(prev_picture):
+		os.remove(prev_picture)
+	picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
+
+	output_size = (125, 125)
+	i = Image.open(form_picture)
+	i.thumbnail(output_size)
+	i.save(picture_path)
+
+	return picture_fn
+
+@app.route("/account", methods=['GET','POST'])
 @login_required
 def account():
-	return render_template("account.html")
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			picture_file = save_picture(form.picture.data)
+			current_user.image_file = picture_file	
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash("Account Updated successfully", 'green')
+		return redirect(url_for('account'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
+	image_file = url_for('static', filename='img/'+current_user.image_file)
+	return render_template("account.html", image_file=image_file, form=form)
