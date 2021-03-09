633bb91d442f85021855df059b273373927afad3
Rahul Chaudhary <rahul.chaudhary.3100@gmail.com>
2021-03-09 19:55:09 +0530

    Blueprints and configurations && Custom error Pages

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
+
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
@@ -46,11 +47,6 @@ class UpdateAccountForm(FlaskForm):
 			if user:
 				raise ValidationError('Email already exists')
 
-
-class PostForm(FlaskForm):
-	title = StringField("Title", validators=[DataRequired()])
-	content = TextAreaField("Content", validators=[DataRequired()])
-	submit = SubmitField("Post")
 
 class RequestResetForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
